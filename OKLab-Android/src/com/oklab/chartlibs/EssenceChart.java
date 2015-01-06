e2cd17c93b681700af3f870532c1901618af6e68
ojungwon <ridsync@gmail.com>
2015-01-06 16:06:23 +0900

    chart lib 로 수정

--- OKLab-Android/src/com/oklab/chartlibs/EssenceChart.java
+++ OKLab-Android/src/com/oklab/chartlibs/EssenceChart.java
@@ -4,6 +4,7 @@ package com.oklab.chartlibs;
  * Created by ojungwon on 2015-01-05.
  * https://github.com/essencetech/essencechart
  * Chart 한개 Lib 클래스로 옵션 설정 다양하고 커스터마이징위한 옵션 방대하게 구현되어있음.
+ * NDK 라이브러리 사용 : JNI 호출로 코어 소스수정 불가.
  */
 public class EssenceChart {
 }

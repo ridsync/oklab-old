d40394d64e0d9efc348d9b84da85eb35c867c062
ojungwon <ridsync@gmail.com>
2015-01-12 10:16:13 +0900

    StrictMode 추가

--- OKLab-Android/src/com/oklab/OKLabApp.java
+++ OKLab-Android/src/com/oklab/OKLabApp.java
@@ -20,6 +20,7 @@ public class OKLabApp extends Application{
         // GB 이상부터 기본적용됨 Cause Main Thread에서 Netowork사용시 Exception 발생
         // REf. http://javaexpert.tistory.com483   &&  http://developer.android.com/reference/android/os/StrictMode.html
 
+        // DetectedAll ^^
 //        if (Constants.Config.DEVELOPER_MODE && Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
 //            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().penaltyDialog().penaltyLog().build());
 //            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().penaltyDeath().penaltyLog().build());

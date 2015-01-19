5ff0f550bb6fb820adb6d170952c5dce5b092674
ridsync <ridsync@naver.com>
2015-01-19 23:20:35 +0900

    웹에서 앱 특정 Activity를 실행 하는 방법

--- OKLab-Android/src/com/oklab/webview/StartAppFromWebSite.java
+++ OKLab-Android/src/com/oklab/webview/StartAppFromWebSite.java
@@ -3,6 +3,7 @@ package com.oklab.webview;
 /**
  * Created by okc on 2015-01-19.
  * Ref : http://developer.android.com/training/basics/intents/filters.html
+ * Ref : http://blog.daum.net/mailss/36
  * Ref : https://ch7895.wordpress.com/2013/01/15/android-url-scheme-%EC%A3%BC%EC%86%8C%EC%B0%BD%EC%97%90%EC%84%9C-%EC%95%B1%EC%8B%A4%ED%96%89/
  1. 실행할 앱의 scheme 정보를 알아야 한다.
  만약 만들고 있거나, 소스코드가 있는 앱이라면 Manifest파일에 다음을 추가 한다.

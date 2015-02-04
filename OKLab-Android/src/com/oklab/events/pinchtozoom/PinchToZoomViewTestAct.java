50844261fb7dabf6bfb2f4ed0ee78b07b15ce1de
ojungwon <ridsync@gmail.com>
2015-02-04 17:51:35 +0900

    수정

--- OKLab-Android/src/com/oklab/events/pinchtozoom/PinchToZoomViewTestAct.java
+++ OKLab-Android/src/com/oklab/events/pinchtozoom/PinchToZoomViewTestAct.java
@@ -9,6 +9,14 @@ import android.widget.ImageView.ScaleType;
 import com.oklab.R;
 import com.oklab.views.MultiZoomImageView;
 
+/**
+ * 1. 핀치 투 줌 (+뷰 드래그 이동)
+ * http://90000e.tistory.com/15
+ * http://aroundck.tistory.com/839 샘플
+ *
+ * 2. 줌인 아웃만 되는 이미지뷰 ??? 이동안됨 왜 ?
+ * http://blog.flysky.kr/blog/4547416 샘플
+ */
 public class PinchToZoomViewTestAct extends Activity {
 	
 	private static final String TAG = "TouchEventsTest";
@@ -19,12 +27,12 @@ public class PinchToZoomViewTestAct extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
-		
-		//1. 핀치 투 줌 (+뷰 드래그 이동)   // http://aroundck.tistory.com/839 샘플
+
+		//1. 핀치 투 줌 (+뷰 드래그 이동)
 		PinchToZoomView ptzView =new PinchToZoomView(this);
 		ptzView.setSelectedImage(m_imgPath);
 		
-		//2. 줌인 아웃만 되는 이미지뷰 ??? 이동안됨 왜 ? // http://blog.flysky.kr/blog/4547416 샘플
+		//2. 줌인 아웃만 되는 이미지뷰 ??? 이동안됨 왜 ?
 //		ZoomImageView img = new ZoomImageView(this);
 		MultiZoomImageView imgView = new MultiZoomImageView(this);
 		imgView.setScaleType(ScaleType.MATRIX);

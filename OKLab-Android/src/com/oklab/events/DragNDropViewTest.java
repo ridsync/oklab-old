f54e15bb1fc98be4b0463ed907c401da16370d68
ojungwon <ridsync@gmail.com>
2015-02-25 15:34:10 +0900

    수정

--- OKLab-Android/src/com/oklab/events/DragNDropViewTest.java
+++ OKLab-Android/src/com/oklab/events/DragNDropViewTest.java
@@ -1,10 +1,13 @@
 package com.oklab.events;
 
 /**
- * TouchMoveViewTest By OnDragListener
- *  Ref : https://github.com/bauerca/drag-sort-listview
+ * DragNDropViewTest - ListView & GridView
+ *  https://github.com/bauerca/drag-sort-listview
+ *
  *  https://github.com/thquinn/DraggableGridView
  *  https://github.com/mrKlar/PagedDragDropGrid
+ *  http://www.pocketmagic.net/complex-android-gridview-with-drag-and-drop-functionality/
+ *
  * Created by ojungwon on 2015-02-04.
  */
 public class DragNDropViewTest {

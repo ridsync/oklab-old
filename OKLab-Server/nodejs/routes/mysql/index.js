e6557a3ee775d28dbcce3c949abc792838f66b58
ridsync <ridsync@gmail.com>
2020-11-08 18:02:24 +0900

    OKLab-server 제거

--- OKLab-Server/nodejs/routes/mysql/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(express){
-  var conn = require('../config/mysql/db')();
-  var route =  require('express').Router();
-
-  route.get('/', function(req, res, next) {
-    res.render('index', { title: 'OK Express Test' });
-  });
-  return route;
-}
